f9178d213b408e20fa5e2455c39f5e3a66ac3012
Piotr Krzesniak <krzesniak.piotr@wp.pl>
2021-05-17 15:29:13 +0200

    add main padding changes

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,11 +12,13 @@ struct ContentView: View {
     @ObservedObject var viewModel: WeatherViewModel
     
     var body: some View {
+        ScrollView(){
         VStack {
             ForEach(viewModel.records) {
                 record in WeatherView(record: record, viewModel: viewModel)
             }
         }.padding()
+        }
     }
 }
 
@@ -26,18 +28,27 @@ struct WeatherView: View {
     var body: some View {
         ZStack {
             RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).stroke()
-            HStack {
+                .frame(height: 150, alignment: .center)
+            GeometryReader { geometry in HStack {
                 Text(verbatim: viewModel.getIcon(record: record)).font(.largeTitle)
-                VStack {
+                    .frame(width: geometry.size.width * 0.20)
+            
+                VStack(alignment: .leading
+            ) {
                     Text(record.cityName)
+                    
                     Text("Temperature: \(record.temperature, specifier: "%.2f")'C")
                         .font(.caption)
-                }
+                }.frame(width: geometry.size.width * 0.6)
                 Text("🔁")
                     .font(.largeTitle)
+                    .frame(height: geometry.size
+                            .height)
+    
                     .onTapGesture {
                         viewModel.refresh(record: record)
                     }
+            }
             }
         }
     }
